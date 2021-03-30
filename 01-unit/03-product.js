e67f21e6576201e85fdab5952dc22d01feba967e
zeeshanyousaf93 <zeeshanyousaf93@gmail.com>
2021-03-30 00:11:27 -0400

    Product Test 4 defined

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -1,7 +1,7 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function product(num1, num2) {
+function product(num1, num2 = 1) {
 return num1 * num2;
 }
 
@@ -34,12 +34,13 @@ try {
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+	var result = product(6);
+	  if (result !== 6) throw new Error('Expected product(6) to be 6. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
   console.log('All tests passed successfully.');
 
 // ==================================================
