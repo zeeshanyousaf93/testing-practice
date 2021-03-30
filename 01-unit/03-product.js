2546aa7b52015b72a3e9afb7cca990315c6ab25b
zeeshanyousaf93 <zeeshanyousaf93@gmail.com>
2021-03-29 23:58:40 -0400

    Product Test 2 defined

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -20,6 +20,8 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return 0 when either of the numbers are 0.
+	var result = product(6, 0);
+	  if (result !== 0) throw new Error('Expected product(6, 0) to be 0. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
